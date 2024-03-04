44f87a2a3fd1372d463f3f0d08b0b60a5fe7e50c
MaxRocco <maxramageuk@gmail.com>
2024-03-04 18:11:19 +0000

    Add custom error pages for information leak prevention

--- app.py
+++ app.py
@@ -21,16 +21,46 @@ login_manager.login_view = 'users.login'
 login_manager.init_app(app)
 
 from models import User
+
+
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
 
 from users.views import users_blueprint
+
 app.register_blueprint(users_blueprint)
+
+
 @app.route('/')
 def index():
     return render_template('main/index.html')
+
+
+@app.errorhandler(400)
+def badRequest_error(error):
+    return render_template('errors/400.html')
+
+
+@app.errorhandler(403)
+def accessForbidden_error(error):
+    return render_template('errors/403.html')
+
+
+@app.errorhandler(404)
+def pageNotFound_error(error):
+    return render_template('errors/404.html')
+
+
+@app.errorhandler(500)
+def internalServer_error(error):
+    return render_template('errors/500.html')
+
+
+@app.errorhandler(503)
+def serviceUnavailable_error(error):
+    return render_template('errors/503.html')
 
 
 if __name__ == '__main__':
